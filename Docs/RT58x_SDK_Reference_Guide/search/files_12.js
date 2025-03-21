1d86f9c0ccf81fd15a0626a7b229282d500b57bb
iveslee308 <ives.lee@rafaelmicro.com>
2025-03-21 09:26:46 +0800

    remove file

--- Docs/RT58x_SDK_Reference_Guide/search/files_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zigbee_5flib_5fapi_2eh_7840',['zigbee_lib_api.h',['../zigbee__lib__api_8h.html',1,'']]]
-];
--- Docs/RT58x_SDK_Reference_Guide/search/typedefs_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['delay_5fms_5ft_9542',['delay_ms_t',['../group___sys___ctrl.html#gafadd26f0b8f24dec33d325902c1d7d97',1,'sysctrl.h']]],
-  ['delay_5fus_5ft_9543',['delay_us_t',['../group___sys___ctrl.html#ga2fdf3dbd5dbceb8442aea4075b6553ce',1,'sysctrl.h']]],
-  ['dma_5fproc_5fcb_5ft_9544',['dma_proc_cb_t',['../group___d_m_a___driver.html#ga3520096c5a0a4994ad4a715824bf006e',1,'dma.h']]]
-];
